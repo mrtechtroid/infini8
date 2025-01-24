889e335589ef3addd2e23bd7319f0dd55f9156b2
Mr Techtroid <64828294+mrtechtroid@users.noreply.github.com>
2025-01-24 23:45:01 +0530

    fix more

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import Image from 'next/image'
+import {AnimatedElement} from "@/components/AnimatedElement";
 gsap.registerPlugin(ScrollTrigger)
 
 const events = [
@@ -19,13 +20,15 @@ export default function Events() {
 
   return (
     <section ref={sectionRef} className="py-20 bg-gray-900 text-white w-full h-full bg-red-900 bg-[url('/eventsbg.jpg')] relative">
-      <img src="/sushi.svg" alt="Background pattern" width={1000} height={1000} className="absolute right-[0%] bottom-[0%] w-[90%] h-[90%]" />
+      <AnimatedElement><img src="/sushi.svg" alt="Background pattern" width={1000} height={1000} className="absolute right-[0%] bottom-[0%] w-[90%] h-[90%]" /></AnimatedElement>
       <div className="container mx-auto px-4 absolute left-[4%] top-[10%] w-[50%] h-[90%]">
-        <h2 className="text-4xl font-bold mb-12" style={{fontFamily:"The Last Shuriken"}}>Events</h2>
+      <AnimatedElement><h2 className="text-4xl font-bold mb-12" style={{fontFamily:"The Last Shuriken"}}>Events</h2></AnimatedElement>
         <span>
+          <AnimatedElement>
           <p className="text-xl text-gray-300 leading-relaxed">
             Infin8 is a three-day extravaganza filled with vibrant shows, performances, competitions, games, and stalls.
           </p>
+          </AnimatedElement>
         </span>
       </div>
     </section>
